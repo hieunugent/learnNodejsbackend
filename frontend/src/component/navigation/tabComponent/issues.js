b81ffc0d54e812b70068c6d8a1412fe2e7b64bff
hieunugent <hieunugent@gmail.com>
2020-07-24 22:28:03 -0700

    delete frontedn d

--- frontend/src/component/navigation/tabComponent/issues.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import AddIssue from './issuesComponent/addIssueButton';
-export default function Issues(){  
-    return (
-        <div>
-            <AddIssue/>
-        </div>
-    );
-}
--- frontend/src/component/navigation/tabComponent/users.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import "./mycomponent.css";
-export default function Users() {
-  return (
-     <div >
-       <h1> Users Start</h1>
-    </div>
-  );
-}
