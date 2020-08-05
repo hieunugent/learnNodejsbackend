f45b950e8f10f48016ffce4bf67bacc3a5155213
hieunugent <hieunugent@gmail.com>
2020-08-04 19:27:54 -0700

    add api for Login/logout/register

--- routes/users.routers.js
+++ routes/users.routers.js
@@ -2,4 +2,12 @@ module.exports = (app) => {
     const users = require("../controllers/users.controller.js");
      var router = require("express").Router();
 
+     // create a new user
+     router.post("/", users.registrations);
+     // login 
+     router.post("/", users.logIn);
+     //logout
+     router.post("/",users.logOut)
+     
+
 }
